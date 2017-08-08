301f3faf61a1c1aeeb9186d1d25b7b860a78d2ea
nilihanth <nilihanth@users.noreply.github.com>
2017-08-08 09:54:08 -0500

    Further adjustments and schema changes

--- server.js
+++ server.js
@@ -4,4 +4,5 @@ var da = require('./src/script/db-access.js');
 // Start HTTP Server
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("Under construction...");
 }).listen(8080);
--- src/script/db-access.js
+++ src/script/db-access.js
@@ -1,17 +1,19 @@
 const sqlite3 = require('sqlite3').verbose();
 
-// Open the database
-var db = new sqlite3.Database('./test.db', sqlite3.OPEN_READWRITE);
-console.log("Connected to the database...");
+var dbAccess = function() {
+  // Open the database
+  var db = new sqlite3.Database('src/db/homeschool.db', sqlite3.OPEN_READWRITE);
+  console.log("Connected to the database...");
 
-// Create timestamps
-var dateMS = Date.now();
-var d = new Date();
-var dateStamp = d.getMonth() +"/"+ d.getDay() +"/"+ d.getFullYear();
+  // Create timestamps
+  var dateMS = Date.now();
+  var d = new Date();
+  var dateStamp = d.getMonth() +"/"+ d.getDay() +"/"+ d.getFullYear();
 
-// Insert into the database
-db.run("");
+  // Insert into the database
+  db.run("");
 
-// Close the database
-db.close;
-console.log("Database connection closed.");
+  // Close the database
+  db.close;
+  console.log("Database connection closed.");
+};
